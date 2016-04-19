f4ae76bb25172809ee758c9c79e5e71ea67d7757
daniellasheva <daniellasheva@gmail.com>
2016-04-19 18:31:17 -0400

    Commit 2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-index.js
+$(function() {
+
+  window.onload = function() {
+     alert( "welcome" );
+ }
+
+//have the letters of header turn to black when hovered
+
+$("submit").click(function( event ) {
+  event.preventDefault();
+});
+
+$("#submitButton").click(function(){
+    alert("Thank you!");
+});
+
+$(".header .a").css("text-shadow: 2px 2px black");
+
+$(".donateButton").click(function() {
+$(".billing").css("display", "block");
+
+});
+
+});
